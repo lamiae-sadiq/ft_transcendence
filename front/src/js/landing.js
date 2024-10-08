3bdbf5fc1eb51830a629a8fd00acc1af90a21bae
Ayoubioussoufa <Ayoubioussoufa@gmail.com>
2024-10-08 10:57:20 +0100

    wf

--- front/src/js/landing.js
+++ front/src/js/landing.js
@@ -2,36 +2,41 @@ import { navigateTo } from "./main.js";
 
 export function initLandingPage() {
     console.log("LANDING PAGE");
-      // This script runs when the page loads
-  window.onload = async function () {
-    // Get the query parameters from the URL
-    const urlParams = new URLSearchParams(window.location.search);
-
-    // Example: Extract a query parameter called 'code'
-    const authCode = urlParams.get("code");
-
-    if (authCode) {
-      console.log("Authorization Code:", authCode);
-      try {
-        const response = await fetch(
-          "http://0.0.0.0:8000/oauthcallback?code=" + authCode
-        );
-        if (response.ok) {
-          console.log("Authentication initiated successfully");
-          console.log(response);
-          let rewind = await response.json();
-          const token = rewind.access; // Replace with actual token retrieval
-          sessionStorage.setItem("jwtToken", token);
-          navigateTo("home"); // to be changed later on
-        } else {
-          console.error("Failed to initiate 42 authentication");
-        }
-      } catch (error) {
-        console.error("Login with 42 failed:", error);
-      }
-    } else {
-      // Handle the absence of the authorization code
-      console.log("No authorization code found.");
-    }
-  };
 }
+
+    // This script runs when the page loads
+    window.onload = async function () {
+        // Get the query parameters from the URL
+        const urlParams = new URLSearchParams(window.location.search);
+        console.log(urlParams);
+    
+        // Example: Extract a query parameter called 'code'
+        const authCode = urlParams.get("code");
+    
+        if (authCode) {
+          console.log("Authorization Code:", authCode);
+          try {
+            console.log("before");
+            const response = await fetch(
+              "http://0.0.0.0:8000/oauthcallback?code=" + authCode
+            );
+            if (response.ok) {
+            console.log("AFTER");
+
+              console.log("Authentication initiated successfully");
+              console.log(response);
+              let rewind = await response.json();
+              const token = rewind.access; // Replace with actual token retrieval
+              sessionStorage.setItem("jwtToken", token);
+              navigateTo("home"); // to be changed later on
+            } else {
+              console.error("Failed to initiate 42 authentication");
+            }
+          } catch (error) {
+            console.error("Login with 42 failed:", error);
+          }
+        } else {
+          // Handle the absence of the authorization code
+          console.log("No authorization code found.");
+        }
+      };
--- project/SmartContract/views.py
+++ project/SmartContract/views.py
@@ -18,9 +18,9 @@ CHAIN_ID = 11155111
 
 # Load ABI and bytecode
 def load_contract_data():
-    with open('/SmartContract/SM/abi.json', 'r') as abifile:
+    with open('/app/SmartContract/SM/abi.json', 'r') as abifile:
         abi_data = json.load(abifile)
-    with open('/SmartContract/SM/bytecode.json', 'r') as bytefile:
+    with open('/app/SmartContract/SM/bytecode.json', 'r') as bytefile:
         bytecode_data = json.load(bytefile)
     return abi_data["abi"], bytecode_data["byteCode"]
 
